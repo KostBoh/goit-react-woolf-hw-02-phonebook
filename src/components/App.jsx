2f2bceb65d08ce1d2a953a0e00e2ab6e21cd5e6a
Kostiantyn Bohdan <bohdan.kostya@gmail.com>
2024-04-10 23:02:13 +0300

    add change step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import CreatePhonebookForm from './Forms/CreatePhonebookForm';
+
+const { Component } = require('react');
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <>
+        <CreatePhonebookForm />
+
+        {/* <div
+          style={{
+            height: '20vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          React homework template
+        </div> */}
+      </>
+    );
+  }
+}
+export default App;
---
+++ src/components/Forms/CreatePhonebookForm/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './CreatePhonebookForm.module.css';
+
+const CreatePhonebookForm = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log(e);
+  };
+
+  const handleChange = ({ target: { value } }) => {
+    console.log(value);
+  };
+  return (
+    <div className="container">
+      <h1>Phonebook</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          value="test"
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and  spaces.        For example Adrian, Jacob Mercer, Charles de  Batz de Castelmore       d'Artagnan"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit">Add contact</button>
+      </form>
+      <h2>Contacts</h2>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default CreatePhonebookForm;
