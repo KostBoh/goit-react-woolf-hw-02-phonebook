c35f2e0d53f9bc6863ea5812ebc6cbb1aae29421
Kostiantyn Bohdan <bohdan.kostya@gmail.com>
2024-04-11 18:08:46 +0300

    Update index.jsx

--- src/components/Forms/CreatePhonebookForm/index.jsx
+++ src/components/Forms/CreatePhonebookForm/index.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './CreatePhonebookForm.module.css';
 
-const CreatePhonebookForm = () => {
-  const handleSubmit = e => {
+class CreatePhonebookForm extends Component {
+  state = {
+    value: '',
+  };
+
+  handleSubmit = e => {
     e.preventDefault();
     // console.log(e);
   };
 
-  const handleChange = ({ target: { value } }) => {
+  handleChange = ({ target: { value } }) => {
     console.log(value);
+    if (value.includes('@')) return;
+    this.setState({ value });
   };
-  return (
-    <div className={styles.container}>
-      <h1>Phonebook</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          value="test"
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and  spaces.        For example Adrian, Jacob Mercer, Charles de  Batz de Castelmore       d'Artagnan"
-          required
-          onChange={handleChange}
-        />
-        <button type="submit">Add contact</button>
-      </form>
-      <h2>Contacts</h2>
-      <ul></ul>
-    </div>
-  );
-};
+  render() {
+    // console.log(this.state.value);
+    return (
+      <div className={styles.container}>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            value={this.state.value}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and  spaces.        For example Adrian, Jacob Mercer, Charles de  Batz de Castelmore       d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul></ul>
+      </div>
+    );
+  }
+}
 
 export default CreatePhonebookForm;
