329da717452749caf87ca236d3979c2b4965aef8
KostBoh <bohdan.kostya@gmail.com>
2024-04-17 18:19:37 +0300

    Update CreatePhonebookForm.jsx

--- src/components/CreatePhonebookForm/CreatePhonebookForm.jsx
+++ src/components/CreatePhonebookForm/CreatePhonebookForm.jsx
@@ -45,7 +45,7 @@ class CreatePhonebookForm extends Component {
             type="tel"
             name="number"
             className={styles.input}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
